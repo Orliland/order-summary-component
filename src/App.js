3d8f35b869c4417bd99cd3abc355e718c4859fd6
Orlando Verdin <git@orli.land>
2024-03-20 22:21:47 -0700

    add basic structure component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import Hero from "./assets/illustration-hero.svg";
+import Music from "./assets/icon-music.svg";
 
-function App() {
-  return <h1>Oder Summary Component</h1>;
+function PlanSelected() {
+  return (
+    <div className="plan">
+      <img src={Music} alt="Icon of plan selected" className="plan__icon" />
+      <div className="plan__body">
+        <h3 className="plan__title">Annual Plan</h3>
+        <p className="plan__amount">$59.99/year</p>
+      </div>
+      <button className="plan__change">Change</button>
+    </div>
+  );
 }
 
-export default App;
+function Order() {
+  return (
+    <main className="card">
+      <img src={Hero} alt="thumbnail of the card" className="card__thumb" />
+      <h2 className="card__title">Order Summary</h2>
+      <p className="card__description">
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhere you like!
+      </p>
+      <PlanSelected />
+      <button className="button button--primary">Proceed to Payment</button>
+      <button className="button button--secondary">Cancel Order</button>
+    </main>
+  );
+}
+
+export default Order;
